351ef507b9562a6083f7927153899661caf29cc9
lacoop6tu <burzi.andrea@gmail.com>
2024-07-10 16:50:42 +0200

    add Kresko TVL

---
+++ projects/kresko/index.js
@@ -0,0 +1,30 @@
+const { request, } = require("graphql-request");
+const subgraphUrl = "https://subgraph.satsuma-prod.com/b3cd6ab10b28/kresko/arbitrum-core/version/0.1.6.20/api"
+const query = `
+query protocolData {
+  history: protocolHistories(
+    first: 1
+    orderBy: blockTimestamp
+    orderDirection: desc
+  ) {
+    tvlUSD
+  }
+}
+`;
+
+async function tvl() {
+  const {history} = await request(subgraphUrl, query);
+
+  return history[0].tvlUSD
+}
+
+
+
+module.exports = {
+  methodology: 'TVL from Kresko Arbitrum subgraph.',
+  start: 188414406,
+  arbitrum: {
+    fetch: tvl
+  },
+  fetch: tvl
+};
